92d7bd07d7c3cc60a9869a961ce3dad0f156d9be
Pepperomai <akuhtarova@gmail.com>
2026-01-25 23:07:18 +0300

    Initial Commit

---
+++ WarriorWorkout/WarriorWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarriorWorkoutApp.swift
+//  WarriorWorkout
+//
+//  Created by Анна Кухтарова on 25.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct WarriorWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
